d3b0a8674c20a8a81bafc707dd4cc5aacd215748
vovinhd <timo.volk@gmail.com>
2022-07-05 09:14:03 +0200

    remove * from todos

--- ingest.py
+++ ingest.py
@@ -1,7 +1,6 @@
 import io
-from tokenize import group
-from numpy import diff
 import pandas as pd
+import re
 
 xl_file = "./Challenges.xlsx"
 table = "Tabelle1"
@@ -16,7 +15,10 @@ merge_cols = [
 def parse_checklist(checklist):
     _checklist = []
     for item in checklist.split("&#10;"):
-        _checklist.append(item.strip())
+        todo = item
+        todo = re.sub(r"\*", " ", todo)
+        todo = todo.strip()
+        _checklist.append(todo)
     return _checklist
 
 
@@ -56,7 +58,6 @@ if __name__ == "__main__":
             selected_df.drop(index, inplace=True)
 
     # create difficulty objects per challenge
-
     difficulty = []
     for index, row in selected_df.iterrows():
         current_difficulties = {}
@@ -64,22 +65,34 @@ if __name__ == "__main__":
             head, *tail = row[merge_col]
             # if head is string replace &#10; with :
             if isinstance(head, str):
+                # &#10; is excels representation of a newline
                 head, *s = head.split("&#10;")
             else:
                 continue
-            print(tail)
             opts = {
-                "Aufgabenbeschreibung": tail[0], "Checkliste": parse_checklist(tail[1]) if len(tail) > 1 else None, "hint": s[0] if s != [] else None}
+                "taskDescription": tail[0],
+                "todos": parse_checklist(tail[1]) if len(tail) > 1 else None,
+                "hint": s[0] if s != [] else None}
             current_difficulties[head] = opts
         difficulty.append(current_difficulties)
 
+    # add a col to dataframe with the difficulty objects
     selected_df["Schwierigkeitsgrade"] = difficulty
 
+    # remove all merged columns
     selected_df = selected_df.drop(merge_cols, 1)
 
+    selected_df.rename(columns={
+        'Titel der Challenge ': 'title',
+        'Impact (Big Point, &#10;easy action,save money)': 'tags',
+        'Beschreibung': 'description',
+        'Hintergrundwissen/ Infobytes': 'background',
+        'Schwierigkeitsgrade': 'difficulty'
+    }, inplace=True)
+
+    # write to json
     out = selected_df.to_json(
         orient='records', indent=True, force_ascii=False)
-    # out = out.encode('utf-8')
 
     with io.open("text.json", mode="w", encoding="UTF8") as fd:
         fd.writelines(out)
